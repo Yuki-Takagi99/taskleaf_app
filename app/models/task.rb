eb297ef18ab8f2f521c59e9c0ae2937e936421b1
yuki takagi <yuki99takagi@gmail.com>
2020-04-22 17:50:13 +0900

    1st commit

---
+++ app/models/task.rb
@@ -0,0 +1,36 @@
+class Task < ApplicationRecord
+  has_one_attached :image
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr) }
+      end
+    end
+  end
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc) }
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+end
